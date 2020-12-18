5e739a0303f0390c6fa3d38d1fa54d478fa305c2
anhnbt-it <anhnbt.it@gmail.com>
2020-12-18 09:30:50 +0700

    First commit

---
+++ src/main/java/com/codegym/controller/PhoneController.java
@@ -0,0 +1,31 @@
+package com.codegym.controller;
+
+import com.codegym.model.Phone;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class PhoneController {
+    @GetMapping("/phone")
+    public String showForm(Model model) {
+        model.addAttribute("phone", new Phone());
+        return "index";
+    }
+
+    @PostMapping("/phone")
+    public String checkValidation(@Valid @ModelAttribute("phone") Phone phone, BindingResult bindingResult, Model model) {
+        new Phone().validate(phone, bindingResult);
+        if (bindingResult.hasFieldErrors()) {
+            return "index";
+        } else {
+            model.addAttribute("phone", phone);
+        }
+        return "result";
+    }
+}
